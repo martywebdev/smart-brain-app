a0e4e1dc851337460efdfecd7627c899f66c1669
Marty Umlas <martyumlas@gmail.com>
2024-08-05 18:28:09 +0800

    add validation

---
+++ src/helper/validation.js
@@ -0,0 +1,29 @@
+export const  isBase64 = (str) => {
+    // Regular expression for validating Base64 string
+    const base64Regex = /^data:image\/[a-zA-Z]+;base64,[A-Za-z0-9+/=]+$/;
+  
+    // Validate format and check if Base64 can be decoded
+    if (!base64Regex.test(str)) return false;
+  
+    const base64Content = str.split(',')[1];
+    try {
+      // Decode Base64 and check if it re-encodes correctly
+      const decoded = atob(base64Content);
+      const encoded = btoa(decoded);
+      return base64Content === encoded;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  
+  export const isValidImageUrl = async (url) => {
+    try {
+      // Check if the URL is valid
+      const response = await fetch(url, { method: 'HEAD' });
+      const contentType = response.headers.get('Content-Type');
+      return response.ok && contentType && contentType.startsWith('image/');
+    } catch {
+      return false;
+    }
+  };
