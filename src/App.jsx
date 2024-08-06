f5ff849fa0b6f2daba50c29745dbc0868b4d8762
Marty Umlas <martyumlas@gmail.com>
2024-08-06 11:42:45 +0800

    update layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navigation from './components/Navigation'
-import Logo from './components/Logo'
+import logo from './assets/logo.png'
 import ImageLinkForm from './components/ImageLinkForm'
 import Rank from './components/Rank'
 import ParticlesBg from 'particles-bg'
@@ -8,23 +8,15 @@ import ParticlesBg from 'particles-bg'
 const App = () => {
   return (
     <>
-    {/* <div style={{ position: 'relative', minHeight: '100vh' }} className='mb4'> */}
-      <Navigation />  
-      <article className="vh-100 dt w-100">
-  {/* <div class="dtc v-mid tc white ph3 ph4-l">
-    <h1 class="f6 f2-m f-subheadline-l fw6 tc">Vertically centering things in css is easy!</h1>
-  </div> */}
-  <div className="dtc v-mid tc ph3 ph4-1">
-  <ImageLinkForm />
-  </div>
-</article>
-
-
-      {/* <Logo /> */}
-      {/* <Rank /> */}
-      {/* <ImageLinkForm /> */}
-      <ParticlesBg type="circle" bg={true}  />
-     {/* </div> */}
+    <header className='shadow-4'>
+      <Navigation />
+    </header>
+    <main class="dt w-100 ">
+    <div class="dtc v-mid tc dark-gray ph3 ph4-l">
+      <h1 class="f1 lh-title tc">Paste Your Image URL</h1>
+      <ImageLinkForm />
+    </div>
+    </main>
     </>
   )
 }
